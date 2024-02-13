e84e091a0ce05c3f498c336cf44b29f1e6cd0d25
dev-tarunbajaj <132070154+dev-tarunbajaj@users.noreply.github.com>
2024-02-13 16:49:55 +0530

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+
+const port = 8080;
+
+const server = http.createServer(function(req, res){
+    res.end("Hello world");
+})
+
+server.listen(port, hostname, function(){
+    console.log(`Server running at ${hostname}:${port}`);
+})
