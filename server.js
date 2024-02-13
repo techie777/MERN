5a560e1a0762b997c338938c2bd4e4a4908405f2
S Singh <techsculptor8@gmail.com>
2024-02-13 17:02:39 +0530

    Port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 
 const hostname = "127.0.0.1";
 
-const port = 8080;
+const port = 5050;
 
 const server = http.createServer(function(req, res){
     res.end("Hello world");
